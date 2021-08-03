73bcdd19bab77e2e8ee24aad79b39a8d53b2057d
Bruno Guedes Rodrigues <brunobgr0810@gmail.com>
2021-08-03 18:21:53 -0300

    Criando função de Validação de disco

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,6 +2,7 @@ let mode = ''; // variável para armazenar o modo torre Origem / torre Destino
 let count = 0;
 let countChild = 1;
 let lastDisc;
+let previousDisc;
 let towerSel;
 
 //Capturando elementos no HTML
@@ -59,15 +60,16 @@ const eventoClick = function(event){
     }
     
     countChild = idTowerSel.childElementCount;
-
+    
     if (countChild > 1){
         lastDisc = idTowerSel.lastElementChild;
+        previousDisc = lastDisc.previousElementSibling; 
     } 
-
+    console.log(mode); // modo de clique: origem ou destino
     console.log(towerSel); // torre clicada
     console.log(countChild); // quantos filho tem a torre
     console.log(lastDisc);  // ultimo filho da torre
-    console.log(mode); // modo de clique: origem ou destino
+    console.log(previousDisc); //penultimo filho da torre
 }
 
 // função de movimento do disco
@@ -85,5 +87,19 @@ function moviment(disc, towerSel){
 contTowerA.addEventListener('click', eventoClick);
 contTowerB.addEventListener('click', eventoClick);
 contTowerC.addEventListener('click', eventoClick);
+
+// função para validar movimento de discos incorretos
+function validMovement(lastDisc, countChild, originDisc){
+    let widthLastDisc = lastDisc.offsetWidth;
+    let widthOrigDisc = originDisc.offsetWidth;
+
+    if (countChild === 1){
+        return true;
+    } else if (countChild > 1 && widthLastDisc < widthOrigDisc){
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // outras funções
